02323254334de7a123447cbda07892df76dbd4f7
Ваше Имя <you@example.com>
2025-04-11 14:06:40 +0700

    проверка на отрицательность

--- rect.cpp
+++ rect.cpp
@@ -6,11 +6,17 @@ int main(){
     float a, b;
     cout << "Введите в одну строчку стороны прямоугольника: ";
     cin >> a >> b;
-    float per, pl, d;
-    per = 2 * a + 2 * b;
-    pl = a * b;
-    d = sqrt(a*a + b*b);
-    cout << "Периметр: " << per << "\n";
-    cout << "Площадь: " << pl << "\n";
-    cout << "Длина диагонали: " << d << "\n";
+    if (a <= 0 || b <= 0) {
+        cout << "Сторона не может быть отрицательной" << endl;
+    }
+    else {
+        float per, pl, d;
+        per = 2 * a + 2 * b;
+        pl = a * b;
+        d = sqrt(a*a + b*b);
+        cout << "Периметр: " << per << "\n";
+        cout << "Площадь: " << pl << "\n";
+        cout << "Длина диагонали: " << d << "\n";
+    }
+    
 }
